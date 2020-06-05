6835a913a3bb1d5d3d707ae670cd16b712e41665
Illia Lytvynov <illial@wix.com>
2020-06-05 10:58:56 +0300

    Lesson 16

---
+++ lesson_16/scripts.js
@@ -0,0 +1,4 @@
+import { SlideShow } from './scripts/slideshow.js';
+const root = document.querySelector('.root');
+const mySlideShow = new SlideShow(root);
+console.log(mySlideShow);
---
+++ lesson_16/scripts/slideshow.js
@@ -0,0 +1,69 @@
+export class SlideShow {
+  constructor(root) {
+    this.root = root;
+    this.slidesElements = [];
+    this.activeSlideIndex = 2;
+    this.fetchData();
+  }
+
+  fetchData() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(
+      'GET',
+      'https://my-json-server.typicode.com/ilyalytvynov/ads-box-server/ads'
+    );
+    xhr.send();
+    xhr.onload = () => {
+      console.log(this); /// SlideShow
+
+      const data = JSON.parse(xhr.response);
+      console.log(typeof data); // object
+      this.render(data);
+    };
+  }
+
+  render(slidesArray) {
+    for (let i = 0; i < slidesArray.length; i++) {
+      console.log(i);
+      const element = this.renderSlideAlt(slidesArray[i]);
+      if (i === this.activeSlideIndex) {
+        element.classList.add('active');
+      }
+      this.root.append(element);
+      this.slidesElements.push(element);
+    }
+    this.root.classList.add('slideshow');
+    console.log('RENDERED!!!!', this.slidesElements);
+  }
+
+  renderSlide(slide) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const title = document.createElement('h4');
+    const description = document.createElement('p');
+
+    description.textContent = slide.description;
+    title.textContent = slide.title;
+    img.src = slide.img;
+
+    li.append(img);
+    li.append(title);
+    li.append(description);
+
+    return li;
+  }
+
+  renderSlideAlt(slide) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <img src="${slide.img}"/>
+      <div class="slide__details">
+        <h4>${slide.title}</h4>
+        <p>${slide.description}</p>
+      </div>
+    `;
+    li.classList.add('slide');
+    return li;
+  }
+}
